e9d5ef3e782815227d0ec9ad398fe3512947cf14
Chuburova Alina <amminami@yandex.ru>
2024-03-29 14:11:59 +0300

    check test test_add_new_book_add_two_books in develop

--- tests.py
+++ tests.py
@@ -1,4 +1,9 @@
+import pytest
 from main import BooksCollector
+
+@pytest.fixture
+def collector():
+    return BooksCollector()
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -8,7 +13,7 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    def test_add_new_book_add_two_books(self, collector):
         # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
@@ -18,7 +23,7 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.books_genre) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
